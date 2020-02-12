5facc5d3a52176c470dce71ce13656e2b9220904
pradeep Kumar <pradeep@gmail.com>
2020-02-12 16:03:37 +0530

    added dashboard

--- src/app/components/doctor/dashboard/dashboard.component.ts
+++ src/app/components/doctor/dashboard/dashboard.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  display: boolean;
+  slots = [
+    { slotName: '10:00 AM', availablity: false },
+    { slotName: '11:00 AM', availablity: false },
+    { slotName: '12:00 AM', availablity: false },
+    { slotName: '2:00 PM', availablity: false },
+    { slotName: '3:00PM', availablity: false }
+  ];
+  selectedSlot = [];
   constructor() { }
 
   ngOnInit() {
+
+
+  }
+
+  makeAvailable() {
+    // console.log(this.slots);
+    // this.selectedSlot = this.slots.filter(
+    //   (res) => {
+    //     return res.availablity === true;
+    //   }
+    // );
+    console.log(this.selectedSlot);
+    this.display = false;
+  }
+
+
+  manageSlots() {
+    this.display = true;
   }
 
 }
